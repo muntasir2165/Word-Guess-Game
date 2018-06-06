bbabb37f14f8daa90cb4700f8e745852112b611e
muntasir2165 <muntasir2165@hotmail.com>
2018-06-06 04:34:40 -0400

    not working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,14 +9,15 @@ page = 1;
 var gameState = {"wins": 0,
 "remainingGuesses": 9,
 "losses": 0,
-"guessedLetters": ""};
+"guessedLetters": [],
+"currentMovie": {}};
 
 $(document).ready(function() {
 	document.onkeyup = function(event) {
     	// console.log(event);
     	// console.log(event.code);
 		//if the space bar button is pressed, start the game
-		if(event.code == "Space" && !hasGameStarted) {
+		if(event.code === "Space" && !hasGameStarted) {
 			hasGameStarted = true;
 			//developer api key
 			var api_key = "87df092609d5d6015362ba701dc095d2";
@@ -34,14 +35,16 @@ $(document).ready(function() {
 			$.ajax({
 				url: url,
 				success: function(result) {
+					// console.log(result);
 					var movieDataArray = result["results"];
+					// console.log(movieDataArray);
 					//intialize the movieArray variable to be an empty array if it wasn't empty already
 					movieArray = [];
 					movieDataArray.forEach(function(movieData){
-						var movie = {};
-						movie.name = movieData["title"];
-						movie.description = movieData["overview"];
-						movieArray.push(movie);
+					var movie = {};
+					movie.movieTitle = movieData["original_title"].toUpperCase();
+					movie.description = movieData["overview"];
+					movieArray.push(movie);
 					});
 					// console.log(movieArray);
 
@@ -67,17 +70,24 @@ function playSet() {
 	// console.log(movieArray.length);
 	while (movieArray.length > 0) {
 		updateGameState();
-		var randomMovieIndex = Math.floor(Math.random() * (movieArray.length - 0)) + 0;
+		// var randomMovieIndex = Math.floor(Math.random() * (movieArray.length - 0)) + 0;
 		// console.log(randomMovieIndex);
-		currentMovie = movieArray.slice(randomMovieIndex, 1);
-		movieArray.splice(randomMovieIndex, 1); //remove the selected movie from the array
-		console.log("The movie name is: " + currentMovie.name);
+		// currentMovie = movieArray.slice(randomMovieIndex, 1);
+		// movieArray.splice(randomMovieIndex, 1); //remove the selected movie from the array
+		currentMovie = movieArray.shift();
+		gameState.currentMovie = currentMovie;
+		console.log("The movie title is: " + currentMovie.movieTitle);
 		playGame(currentMovie);
 	}
 }
 
 function playGame(currentMovie) {
-
+	document.onkeyup = function(event) {
+		if (event.code !== "Space") {
+			gameState.guessedLetters.push(event.code);
+			currentWordState();
+		}
+	}
 }
 
 // update the state of the game and display it in the user interface
@@ -88,4 +98,24 @@ function updateGameState() {
 	losses.innerHTML = gameState.losses;
 	var remainingGuesses = document.getElementById("remaining-guesses");
 	remainingGuesses.innerHTML = gameState.remainingGuesses;
+}
+
+function currentWordState() {
+	var wordDisplayContainer = document.getElementById("word-container");
+	var word = "";
+	for (var i = 0; i < gameState.currentMovie.movieTitle.length; i++) {
+		
+		for (var j = 0; j < gameState.guessedLetters.length; j++) {
+			if (gameState.guessedLetters.charAt(j) === letter) {
+				word += letter;
+				break;
+			} else {
+				word += "-";
+
+			}
+		}
+	} 
+	// console.log("here");
+	wordDisplayContainer.innerHTML = word;
+
 }
